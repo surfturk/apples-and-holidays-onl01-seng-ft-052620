a5c76eca35ec10e8dedd61e7de46cfe04c497bb5
Berkley Brown <berkleydeane@gmail.com>
2020-06-04 13:59:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -89,7 +89,7 @@ holiday_hash.collect do |season, holiday|
  puts season.to_s.capitalize! + ":"
  holiday.collect do |celebration, supplies|
    binding.pry
-   puts "  " + celebration.capitalize!split.join + ": " + supplies.join(", ") 
+   puts "  " + celebration.capitalize.split.join + ": " + supplies.join(", ") 
   
   end 
   
