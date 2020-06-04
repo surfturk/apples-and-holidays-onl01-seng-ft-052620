748eb9a3fa425f864960059c36dbfbdb58ce8276
Berkley Brown <berkleydeane@gmail.com>
2020-06-04 16:17:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -105,8 +105,8 @@ def all_holidays_with_bbq(holiday_hash)
 
 
 holiday_hash.map do |season, holiday|
-    holiday.map do |holiday, item|
-      holiday if item.include?("BBQ")
+    holiday.map do |holiday, supply|
+      holiday if supply.include?("BBQ")
     end 
   end.flatten.compact
 
